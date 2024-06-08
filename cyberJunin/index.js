de62504b9e6405f193c9cac495f911de8a9f30a8
SAMUEL SOBREIRA DA SILVA <samuel.sobreira@a.ucb.br>
2024-06-08 10:36:30 -0300

    atualização sabado manha

--- cyberJunin/index.js
+++ cyberJunin/index.js
@@ -36,6 +36,10 @@ app.get('/ferramentas',function(req,res){
     res.render('ferramentas.html')
 })
 
+app.get('/verificacaourl',function(req,res){
+    res.render('verificacao-url.html')
+})
+
 
 app.get('/noticias',function(req,res){
     res.render('noticias.html')
